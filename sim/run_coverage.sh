de505aa6e81638cca8eb5740da79aed8f6c799fe
phuongdinhnguyen <38172573+phuongdinhnguyen@users.noreply.github.com>
2025-01-21 15:45:56 +0700

    add script for coverage

---
+++ sim/run_coverage.sh
@@ -0,0 +1,8 @@
+reset
+
+rm -rf work
+mkdir -p ucdb
+
+vlog +incdir+./seq +incdir+./src +incdir+./test +incdir+../hdl +cover=bcefs -nocoverfec uart_tb.sv
+
+vsim -c uart_top_testbench -wlf vsim.wlf -voptargs=+acc -sva -assertdebug -fsmdebug -coverage -do "coverage save -onexit -code bcefs -directive -assert -cvg ucdb/coverage.ucdb; add wave -r /*; run -all; quit" 
